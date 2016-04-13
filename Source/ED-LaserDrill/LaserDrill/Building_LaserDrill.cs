5db5575c559db5cbb560cdd36023853d54d213af
Jaxxa <jaxxa90@gmail.com>
2016-04-14 07:52:22 +1000

    Initial Setup

---
+++ Source/ED-LaserDrill/LaserDrill/Building_LaserDrill.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace Enhanced_Development.Power.LaserDrill
+{
+
+    public class Building_LaserDrill : Building
+    {
+        private int drillWork = 500;
+        private CompPowerTrader _PowerComp;
+        private CompFlickable _FlickComp;
+
+        public override void SpawnSetup()
+        {
+            base.SpawnSetup();
+            this._PowerComp = this.GetComp<CompPowerTrader>();
+            this._FlickComp = this.GetComp<CompFlickable>();
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.LookValue<int>(ref this.drillWork, "drillWork", 0, false);
+        }
+
+        public override void TickRare()
+        {
+            if (this._PowerComp.PowerOn)
+            {
+                //Log.Message("Reducing count");
+                this.disableOthers();
+                this.drillWork = this.drillWork - 1;
+            }
+            else
+            {
+                //Log.Message("No Power for drill.");
+            }
+
+            if (this.drillWork <= 0)
+            {
+                Messages.Message("SteamGeyser Created.", MessageSound.Benefit);
+
+                this.Destroy(DestroyMode.Vanish);
+                GenSpawn.Spawn(ThingDef.Named("SteamGeyser"), this.Position);
+            }
+            base.Tick();
+        }
+
+        public void disableOthers()
+        {
+            //<Pawn>(t => t.Position.WithinHorizontalDistanceOf(this.Position, this.MAX_DISTANCE));               
+            IEnumerable<Building> LaserBuildings = Find.ListerBuildings.allBuildingsColonist.Where<Building>(t => t.def.defName == "LaserDrill");
+
+            if (LaserBuildings != null)
+            {
+                //List<Thing> fireTo
+                foreach (Building_LaserDrill currentBuilding in LaserBuildings.ToList())
+                {
+                    //Log.Message("Checking");
+                    if (currentBuilding != this)
+                    {
+                        //if (currentBuilding._PowerComp.DesirePowerOn)
+                        if (currentBuilding._FlickComp.SwitchIsOn)
+                        {
+                            Messages.Message("Only One Laser Drill Can be active at a Time.", MessageSound.Negative);
+                            //currentBuilding.powerComp.DesirePowerOn = false;
+
+                            // currentBuilding._PowerComp.PowerOn = false;
+
+                            currentBuilding._FlickComp.DoFlick();
+
+                           // currentBuilding.powerComp. = false;
+                        }
+
+                    }
+                }
+            }
+            else
+            {
+               // Log.Error("List Null");
+
+            }
+                             
+        }
+
+        public override string GetInspectString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            string text;
+
+            if (_PowerComp != null)
+            {
+                if (this._PowerComp.PowerOn)
+                {
+                    text = "Drill Status: Online";
+
+                }
+                else
+                {
+                    text = "Drill Status: Low Power";
+                }
+            }
+            else
+            {
+                text = "Drill Status: Low Power";
+            }
+
+            stringBuilder.AppendLine(text);
+
+            text = "Drill Work Remaining: " + this.drillWork;
+            stringBuilder.AppendLine(text);
+
+            if (_PowerComp != null)
+            {
+                text = _PowerComp.CompInspectStringExtra();
+                if (!text.NullOrEmpty())
+                {
+                    stringBuilder.AppendLine(text);
+                }
+            }
+
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Source/ED-LaserDrill/LaserDrill/Building_LaserFiller.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Enhanced_Development.Power.LaserDrill
+{
+
+    public class Building_LaserFiller : Building
+    {
+        private static Texture2D UI_ACTIVATE_GATE;
+        private int drillWork = 500;
+        private CompPowerTrader powerComp;
+        Thing targetSteamGeyser = null;
+        bool active = false;
+
+        public override void SpawnSetup()
+        {
+            base.SpawnSetup();
+            this.powerComp = this.GetComp<CompPowerTrader>();
+
+            UI_ACTIVATE_GATE = ContentFinder<Texture2D>.Get("UI/nuke", true);
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.LookValue<int>(ref this.drillWork, "drillWork", 0, false);
+            Scribe_Values.LookValue<bool>(ref this.active, "active", false, false);
+            //Scribe_Values.LookValue<Thing>(ref this.targetSteamGeyser, "targetSteamGeyser", null, false);
+        }
+
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            //Add the stock Gizmoes
+            foreach (var g in base.GetGizmos())
+            {
+                yield return g;
+            }
+
+            if (true)
+            {
+                Command_Action act = new Command_Action();
+                //act.action = () => Designator_Deconstruct.DesignateDeconstruct(this);
+                act.action = () => this.ActivateDrill();
+                act.icon = UI_ACTIVATE_GATE;
+                act.defaultLabel = "Activate Drill";
+                act.defaultDesc = "Activate Drill";
+                act.activateSound = SoundDef.Named("Click");
+                //act.hotKey = KeyBindingDefOf.DesignatorDeconstruct;
+                //act.groupKey = 689736;
+                yield return act;
+            }
+        }
+
+        public Thing FindClosestGuyser()
+        {
+            List<Thing> steamGeysers = Find.ListerThings.ThingsOfDef(ThingDefOf.SteamGeyser);
+            Thing currentLowestGuyser = null;
+
+            double lowestDistance = double.MaxValue;
+
+            foreach (Thing currentGuyser in steamGeysers)
+            {
+                //if (currentGuyser.SpawnedInWorld)
+                if (currentGuyser.Spawned)
+                {
+                    if (this.Position.InHorDistOf(currentGuyser.Position, 5))
+                    {
+                        double distance = Math.Sqrt(Math.Pow((this.Position.x - currentGuyser.Position.x), 2) + Math.Pow((this.Position.y - currentGuyser.Position.y), 2));
+
+                        if (distance < lowestDistance)
+                        {
+
+                            lowestDistance = distance;
+                            currentLowestGuyser = currentGuyser;
+                        }
+                    }
+                }
+            }
+            return currentLowestGuyser;
+        }
+
+        public void ActivateDrill()
+        {
+            this.targetSteamGeyser = this.FindClosestGuyser();
+            this.active = true;
+        }
+
+        //public void RemoveGuyser()
+        //{
+        //    List<Thing> steamGeysers = Find.ListerThings.ThingsOfDef(ThingDefOf.SteamGeyser);
+
+        //    foreach (Thing currentGuyser in steamGeysers)
+        //    {
+        //        currentGuyser.DeSpawn();
+        //    }
+        //}
+
+        public override void TickRare()
+        {
+            if (this.active)
+            {
+                if (this.powerComp.PowerOn)
+                {
+                    Log.Message("Reducing count");
+                    this.drillWork = this.drillWork - 1;
+                }
+                else
+                {
+                    Log.Message("No Power for drill.");
+                }
+
+                if (this.drillWork <= 0)
+                {
+                    //Thing SteamGeyser = Find.ThingGrid.ThingAt(this.Position, ThingDef.Named("SteamGeyser"));
+                    //SteamGeyser.Destroy(DestroyMode.Vanish);
+                    //SteamGeyser.DeSpawn();
+                    if (this.targetSteamGeyser != null)
+                    {
+                        this.targetSteamGeyser.DeSpawn();
+                        this.Destroy(DestroyMode.Vanish);
+                        //GenSpawn.Spawn(ThingDef.Named("SteamGeyser"), this.Position);
+                    }
+                    else
+                    {
+                        this.targetSteamGeyser = this.FindClosestGuyser();
+
+                        if (this.targetSteamGeyser != null)
+                        {
+                            this.targetSteamGeyser.DeSpawn();
+                        }
+                        this.Destroy(DestroyMode.Vanish);
+                    }
+                }
+            }
+            base.Tick();
+        }
+
+        public override string GetInspectString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            string text;
+            if (this.active)
+            {
+                if (powerComp != null)
+                {
+                    if (this.powerComp.PowerOn)
+                    {
+                        text = "Fill Status: Online";
+
+                    }
+                    else
+                    {
+                        text = "Fill Status: Low Power";
+                    }
+                }
+                else
+                {
+                    text = "Fill Status: Low Power";
+                }
+            }
+            else
+            {
+                text = "No Steam Geyser found";
+            }
+
+
+            stringBuilder.AppendLine(text);
+
+            text = "Fill Work Remaining: " + this.drillWork;
+            stringBuilder.AppendLine(text);
+
+            if (powerComp != null)
+            {
+                text = powerComp.CompInspectStringExtra();
+                if (!text.NullOrEmpty())
+                {
+                    stringBuilder.AppendLine(text);
+                }
+            }
+
+
+            return stringBuilder.ToString();
+        }
+    }
+}
